c56ad8829f822320912405c98254ebd4c25d2f40
Hayden Bruin <bruinhayden@gmail.com>
2019-05-26 22:01:07 +1200

    Adding landscaping photos, loading screens, custom html

--- src/components/global-styles.js
+++ src/components/global-styles.js
@@ -1,9 +1,10 @@
-import { createGlobalStyle} from "styled-components"
+import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Montserrat:400,600,700&display=swap');
 
-    body {
+    html, body {
+        background: #000;
         margin: 0;
         padding: 0;
         font-family: 'Montserrat', sans-serif;
--- src/components/templates/default.js
+++ src/components/templates/default.js
@@ -4,12 +4,29 @@ import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import GlobalStyles from '../global-styles'
 
+// Verified Images - https://source.unsplash.com/random/1920x1080?nature
+import landscape1 from '../../images/landscape-1.jpg'
+import landscape2 from '../../images/landscape-2.jpg'
+import landscape3 from '../../images/landscape-3.jpg'
+
 export default class Layout extends Component {
+    getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
     render() {
+        const random = this.getRandomInt(3);
+        let imageUrl = "";
+
+        switch (random) {
+            case 1: { imageUrl = landscape2; break; }
+            case 2: { imageUrl = landscape3; break; }
+            default: { imageUrl = landscape1; break; }
+        }
         return (
             <Fragment>
                 <GlobalStyles />
-                <MeetingRoom style={{ backgroundImage: `url(https://source.unsplash.com/random/1920x1080?nature,water)` }}>
+                <MeetingRoom style={{ backgroundImage: `url(${imageUrl})` }}>
                     {this.props.children}
                 </MeetingRoom>
             </Fragment >
